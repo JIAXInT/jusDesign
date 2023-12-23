b75f58a5b41633bbe3d8733323b4825d00ae22dd
justic <1131918725@qq.com>
2023-12-23 10:03:41 +0800

    feat: init project

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    class="j-button"
+    :class="{
+      [`j-button--${type}`]: type,
+      [`j-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <span><slot></slot></span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonProps } from "./type";
+
+defineProps<ButtonProps>();
+</script>
+
+<style scoped></style>
