88be29659532cec6d71f99864c24ba23dbbcc3f0
justic <1131918725@qq.com>
2024-01-04 12:44:47 +0800

    docs: create docs

---
+++ docs/demo/Button/Basic.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from '@/components/Button/Button.vue';
+</script>
+
+<template>
+  <Button>hello</Button>
+  <Button type="primary">primary</Button>
+  <Button type="danger"> danger</Button>
+  <Button loading>loading</Button>
+</template>
